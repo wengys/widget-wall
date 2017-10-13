a8985cc7864514d1da719e40fbd2be1e3a2560e5
wengys <wengyuansheng@hotmail.com>
2017-10-14 06:47:37 +0800

    修正引用问题

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 export * from "./LayoutMode";
 export * from "./WidgetConfig";
+export * from "./WidgetInstance";
 export * from "./WidgetContainer";
 export * from "./WidgetRuntime";
 export * from "./WidgetRuntimeEvents";
