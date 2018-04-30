4fb08332116b75cfc561e05b29962ea6046918bf
wengys <wengyuansheng@hotmail.com>
2018-04-30 13:39:08 +0800

    es6 class改为通过babel编译

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./LayoutMode";
-export * from "./WidgetContainer";
-export * from "./WidgetRuntimeEvents";
-export * from "./WidgetsFactory";
-//# sourceMappingURL=index.js.map
--- src/WidgetContainer.ts
+++ src/WidgetContainer.ts
@@ -1,4 +1,4 @@
-﻿import * as $ from "jquery"
+﻿import $ from "jquery"
 import { WidgetsFactory } from "./WidgetsFactory"
 import { WidgetContainerConfig, WidgetConfig } from "./WidgetConfig";
 import { WidgetInstance, WidgetDefinition } from "./WidgetInstance";
